f6fc25a72941669d9443e168579343c5d2dd8a32
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2022-11-29 10:07:29 -0500

    Update Footer.tsx

--- components/ui/Footer/Footer.tsx
+++ components/ui/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import s from './Footer.module.css';
 
-import Logo from 'components/icons/Logo';
+import Logo from 'components/icons/ifstar';
 import GitHub from 'components/icons/GitHub';
 
 export default function Footer() {
@@ -14,7 +14,7 @@ export default function Footer() {
               <span className="rounded-full border border-zinc-700 mr-2">
                 <Logo />
               </span>
-              <span>ACME</span>
+              <span>IFTIIN</span>
             </a>
           </Link>
         </div>
@@ -86,17 +86,11 @@ export default function Footer() {
       </div>
       <div className="py-12 flex flex-col md:flex-row justify-between items-center space-y-4 bg-zinc-900">
         <div>
-          <span>&copy; 2020 ACME, Inc. All rights reserved.</span>
+          <span>&copy; 2022 IFTIIN Technologies. All rights reserved.</span>
         </div>
         <div className="flex items-center">
-          <span className="text-white">Crafted by</span>
-          <a href="https://vercel.com" aria-label="Vercel.com Link">
-            <img
-              src="/vercel.svg"
-              alt="Vercel.com Logo"
-              className="inline-block h-6 ml-4 text-white"
-            />
-          </a>
+          
+      
         </div>
       </div>
     </footer>
