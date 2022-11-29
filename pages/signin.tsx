adb496b703fb4eecddebfcfa83fce9465515f113
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2022-11-29 14:00:26 -0500

    Update signin.tsx

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -5,6 +5,7 @@ import LoadingDots from 'components/ui/LoadingDots';
 import Logo from 'components/icons/top.png';
 import { getURL } from '@/utils/helpers';
 import { Auth, ThemeSupa } from '@supabase/auth-ui-react';
+import Image from "next/image";
 
 const SignIn = () => {
   const router = useRouter();
@@ -22,7 +23,11 @@ const SignIn = () => {
       <div className="flex justify-center height-screen-helper">
         <div className="flex flex-col justify-between max-w-lg p-3 m-auto w-80 ">
           <div className="flex justify-center pb-12 ">
-            <Logo width="64px" height="64px" />
+           <Image
+      src={Logo}
+      width="64px"
+      height="64px"
+    />
           </div>
           <div className="flex flex-col space-y-4">
             <Auth
